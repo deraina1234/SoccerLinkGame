632a7fe283c0843ea56e33cc3b5f5280ad88d128
devr1234 <rainadevang@gmail.com>
2026-04-21 00:11:36 -0700

    Link Game Final

--- rules.js
+++ rules.js
@@ -1,36 +1,65 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title); 
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); 
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
-        
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+        let locationData = this.engine.storyData.Locations[key]; 
+
+        // my interactive mechanism is where the coach will give new advice everytime you ask him by randomzing through an array using javascript math functions
+        // first i check for the advice array in the location the player is at
+        if("Advice" in locationData){
+            // store one of the advice pieces into a variable that will be displayed
+            let pieceOfAdvice = locationData.Advice[Math.floor(Math.random() * locationData.Advice.length)];
+            this.engine.show("Here is my advice for you: " + pieceOfAdvice);
+        }
+        // locations without advice follow what they have in their body
+        else{
+            this.engine.show(locationData["Body"]);
+        }
+
+        if("Choices" in locationData){
+            for (let choice of locationData["Choices"]){
+                this.engine.addChoice(choice.Text, choice);
             }
-        } else {
-            this.engine.addChoice("The end.")
+        }
+        else{
+            this.engine.addChoice("The End.")
         }
     }
 
     handleChoice(choice) {
-        if(choice) {
-            this.engine.show("&gt; "+choice.Text);
+        // this is what i adjusted the most, for my lock and key puzzle to work
+        if(choice){
+            if(choice.hasKey){
+                this.engine.playerKey = choice.hasKey;
+            }
+            
+            // finds the location of where the player wants to go to, storing it in keyData
+            let keyData = this.engine.storyData.Locations[choice.Target];
+
+            // checks if the location needs a key, in my case only right wing, and if the player does NOT have the key
+            if(keyData.NeedsKey && this.engine.playerKey !== "longThrowKey"){
+                // i used this logic because there are a lot more locations that don't need a key that do
+                // if this is true, the game will show a message saying you missed a pass and send you back to the center field screen
+                this.engine.show(keyData.GameOver);
+                this.engine.gotoScene(Location, keyData.GameOverTarget);
+                return;
+            }
+            // if not true, go to the scene the player is requesting to go to
             this.engine.gotoScene(Location, choice.Target);
-        } else {
+        }
+        else{
             this.engine.gotoScene(End);
         }
+        
     }
 }
 
@@ -38,6 +67,11 @@ class End extends Scene {
     create() {
         this.engine.show("<hr>");
         this.engine.show(this.engine.storyData.Credits);
+
+
+        let locationData = this.engine.storyData.Locations[key];
+
+
     }
 }
 
